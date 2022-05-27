877fed5b2830841d4e8031f6b3b6d71a11c162de
Simon MacDonald <simon@Chriss-MacBook-Pro.local>
2022-05-26 20:06:10 -0700

    Anchor skin themes against left side of screen and show related champions on hover.

---
+++ src/SkinThemeSet.tsx
@@ -0,0 +1,54 @@
+import { Popover, Typography } from '@mui/material';
+import React from 'react';
+import skinThemeSets from './skinThemeSets';
+
+const SkinThemeSet = (props: { theme: string }) => {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+
+  const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
+  const { theme } = props;
+
+  return (
+    <>
+      <Typography
+        aria-owns={open ? 'mouse-over-popover' : undefined}
+        aria-haspopup="true"
+        onMouseEnter={handlePopoverOpen}
+        onMouseLeave={handlePopoverClose}
+      >
+        {theme}
+      </Typography>
+      <Popover
+        id="mouse-over-popover"
+        sx={{
+          pointerEvents: 'none',
+        }}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+      >
+        <Typography sx={{ p: 1 }}>{skinThemeSets[theme].join(' ')}</Typography>
+      </Popover>
+    </>
+  );
+};
+
+export default SkinThemeSet;
