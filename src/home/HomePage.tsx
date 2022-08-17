18d0c9694c02653381b182c2c9ae35dbfe502338
simon <simon@Simons-MBP.dev>
2022-08-17 12:13:35 +0100

    New top row for displaying selected champions.

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -14,19 +14,35 @@ const HomePage = () => {
   const champions = useAppSelector(selectChampions);
   const display = useAppSelector(selectDisplay);
 
+  const champGridItemSizes = {
+    xs: 30,
+    sm: 20,
+    md: 12,
+    lg: 10,
+    xl: 6,
+  };
+
   return (
     <Container>
       <Toolbar>
         {}
       </Toolbar>
+      {display.champions.length > 0 && (
+      <Grid container spacing={2} columns={60}>
+        {display.champions.map((championId) => (
+          <PortraitCard id={championId} sizes={champGridItemSizes} />
+        ))}
+      </Grid>
+      )}
       <Grid container spacing={2} columns={60}>
         {champions.ids
           .filter((id) => id > 0)
           .filter((id) => display.displays[+id] !== 'hidden')
+          .filter((id) => display.displays[+id] !== 'chosen')
           .map((id) => (
-            <PortraitCard id={+id} key={id} />
+            <PortraitCard id={+id} key={id} sizes={champGridItemSizes} />
           ))}
-        {display.champions.length > 0 && <PortraitCard id={-1} />}
+        {display.champions.length > 0 && <PortraitCard id={-1} sizes={champGridItemSizes} />}
       </Grid>
     </Container>
   );
