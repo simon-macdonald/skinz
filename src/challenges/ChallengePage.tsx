2f596dc1d580cdb2d5b774dbba9acf7bd9126d89
Simon MacDonald <simon@MacBook.air>
2022-10-20 17:12:45 -0700

    Bump right.

--- src/challenges/ChallengePage.tsx
+++ src/challenges/ChallengePage.tsx
@@ -20,14 +20,8 @@ const trackedChallenges = [
   'Master Yourself',
   'Master the Enemy',
 ];
-// we want https://raw.communitydragon.org/latest/game/assets/challenges/config/202303/tokens/challenger.png
-// we have                       /lol-game-data/assets/ASSETS/Challenges/Config/101105/Tokens/CHALLENGER.png
 
-const getChallengeIconPath = (path: string) => {
-  const x = `https://raw.communitydragon.org/latest/game/${path.toLowerCase().replace('/lol-game-data/assets/', '')}`;
-  console.log(x)
-  return x
-}
+const getChallengeIconPath = (path: string) => `https://raw.communitydragon.org/latest/game/${path.toLowerCase().replace('/lol-game-data/assets/', '')}`
 
 const ChallengePage = () => {
   const challenges = useAppSelector(selectChallenges);
@@ -40,34 +34,35 @@ const ChallengePage = () => {
       </Toolbar>
       <TableContainer component={Paper}>
         <Table stickyHeader>
-        <TableHead>
-          <TableRow>
-            <TableCell>Challenge</TableCell>
-            {trackedChallenges.map((c) => (
-              <TableCell align="right">
-                <Tooltip title={c}>
-                  <Avatar src={getChallengeIconPath(challenges.entities[c]!.levelToIconPath.CHALLENGER)} />
-                </Tooltip>
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {champions.ids.map((id) => champions.entities[id]).map((c) => (
-            <TableRow
-              key={c!.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell>{c!.name}</TableCell>
+          <TableHead>
+            <TableRow>
+              <TableCell>Challenge</TableCell>
               {trackedChallenges.map((c) => (
-                <TableCell>
-                  {/* <Button variant="text">Text</Button> */}
-                  <Checkbox />
+                <TableCell align="right">
+                  <Tooltip title={c}>
+                    <Avatar src={getChallengeIconPath(challenges.entities[c]!.levelToIconPath.CHALLENGER)} />
+                  </Tooltip>
                 </TableCell>
               ))}
             </TableRow>
-          ))}
-        </TableBody>
+          </TableHead>
+          <TableBody>
+            {champions.ids.map((id) => champions.entities[id]).map((c) => (
+              <TableRow
+                key={c!.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell>{c!.name}</TableCell>
+                {trackedChallenges.map((c) => (
+                  <TableCell>
+                    <Tooltip title={c}>
+                      <Checkbox />
+                    </Tooltip>
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
         </Table>
       </TableContainer>
     </Container>
