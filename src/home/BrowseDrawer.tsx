73442518fda7138b9804874646274ea339a5c73b
Simon MacDonald <simon@MacBook.air>
2022-10-23 16:34:13 -0700

    Only render BrowseDrawer on skin/chroma page.

--- src/home/BrowseDrawer.tsx
+++ src/home/BrowseDrawer.tsx
@@ -14,6 +14,7 @@ import {
 import { selectSkinLines } from '../skins/skinLineSlice';
 import { selectColors } from '../chromas/colorSlice';
 import ColorHoverLink from '../chromas/ColorHoverLink';
+import { useLocation } from 'react-router-dom';
 
 interface TabPanelProps {
   children: React.ReactNode;
@@ -60,7 +61,9 @@ const BrowseDrawer = () => {
     dispatch(clickChamp(-1));
   };
 
-  if (!display.showDrawer) {
+  const location = useLocation();
+
+  if (!display.showDrawer || ['/about', '/challenges'].includes(location.pathname)) {
     return null;
   }
 
