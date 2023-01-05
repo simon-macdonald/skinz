65cf00dfaf80ecee0ddee5c3f57089b917150001
Simon MacDonald <simon@MacBook.air>
2023-01-04 22:34:31 -0800

    Add inverse maps for skin line -> universe and the inverse.

--- scripts/updateSkinLineUniverses.py
+++ scripts/updateSkinLineUniverses.py
@@ -7,14 +7,26 @@ url = 'https://leagueoflegends.fandom.com/wiki/Module:SkinData/universes?action=
 data = urlopen(url).read().decode('UTF-8')
 lines = data.splitlines(False)
 
-universes = {}
+universe_to_skin_lines = {}
+skin_line_to_universe = {}
 
 for line in lines[3:-4]:
-  left, right = line.split('=')
-  universes[left.strip()[2:-2]] = [x[1:-1] for x in right.strip()[1:-2].split(', ')]
-print(universes)
+  universe, skin_lines = line.split('=')
+  universe = universe.strip()[2:-2]
+  skin_lines = [skin_line[1:-1] for skin_line in skin_lines.strip()[1:-2].split(', ')]
+  universe_to_skin_lines[universe] = skin_lines
+  for skin_line in skin_lines:
+    if skin_line in skin_line_to_universe.keys():
+      print('The following skin line is represented in multiple universes:')
+      print('\t' + skin_line)
+    skin_line_to_universe[skin_line] = universe
 
-jsonDump = json.dumps(universes, indent=2, sort_keys=True)
+json_dump = json.dumps(universe_to_skin_lines, indent=2, sort_keys=True)
 
-with open('./src/skins/universes.json','w') as f:
-    f.write(jsonDump)
+with open('./src/skins/universe_to_skin_lines.json','w') as f:
+    f.write(json_dump)
+
+json_dump = json.dumps(skin_line_to_universe, indent=2, sort_keys=True)
+
+with open('./src/skins/skin_line_to_universe.json','w') as f:
+    f.write(json_dump)
