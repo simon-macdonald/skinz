84d7e40e3441908cb1958a77438b33278565b31a
Simon MacDonald <simon@MacBook.air>
2024-11-03 16:45:31 +0900

    Fixed Hwei image.

--- src/urls.ts
+++ src/urls.ts
@@ -6,6 +6,9 @@ export const getChampionTileUrl = (championAlias: string) => {
   if (['aurora'].includes(championAlias.toLowerCase())) {
     return `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/characters/${championAlias}/skins/base/images/${championAlias}_splash_tile_0.${championAlias}.jpg`.toLowerCase();
   }
+  if (['hwei'].includes(championAlias.toLowerCase())) {
+    return `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/characters/${championAlias}/skins/skin0/images/${championAlias}_splash_tile_0.jpg`.toLowerCase();
+  }
   return `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/characters/${championAlias}/skins/base/images/${championAlias}_splash_tile_0.jpg`.toLowerCase();
 };
 
